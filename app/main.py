591cf89a75386a06cc1a1756ec240e99d7100c14
Egor Pluzhnik <e.pluzhnik@tinkoff.ru>
2023-08-26 18:12:52 +0300

    fix contract

--- app/main.py
+++ app/main.py
@@ -10,13 +10,13 @@ from app.src.models import AnswerRequest
 
 app = FastAPI()
 
+
 @app.post("/answer")
 async def read_root(request: AnswerRequest):
-    return {"answer": f'Ответ на {request.question}'}
+    return {[{"question": 'вопрос', "answer": 'ответ'}]}
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     uvicorn.run(app, host="0.0.0.0", port=8000)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
